372410adf2dd0adb09c41f2e9122ba01d826abde
Anthony Touchet <anthony.touchet@students.aie.edu.au>
2016-08-22 09:03:20 -0500

    Creating Identiy Matrix

---
+++ Identity Matrix/Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main() {
+
+	const int size = 6;
+
+	float matrix[size][size];
+
+	int oneSpot = 0;
+
+	for (int i = 0; i < size; i++) {
+		for (int ii = 0; ii < size; ii++) {
+			if (i == oneSpot && ii == oneSpot) {
+				matrix[i][ii] = 1;
+				oneSpot++;
+			}
+
+			else {
+				matrix[i][ii] = 0;
+			}
+		}
+	}
+
+	for (int i = 0; i < size; i++) {
+		for (int ii = 0; ii < size; ii++) {
+			std::cout << matrix[i][ii] << " ";
+		}
+
+		std::cout << std::endl;
+	}
+
+	system("pause");
+	return 0;
+}
